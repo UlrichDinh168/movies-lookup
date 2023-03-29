25efca07d8825f464a072964cfd07de6906b3cf7
Ulrich Dinh <duong0028@gmail.com>
2023-03-29 23:59:58 +0300

    update yml + output.tf

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,7 @@
 output "cine_react_bucket_name" {
   value = aws_s3_bucket.cine_react_s3_bucket.id
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
