42938b9083dbcd3f34d9b416108b0ce549ce01c2
Ulrich Dinh <duong0028@gmail.com>
2023-04-03 22:53:23 +0300

    fix config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {}
   },
