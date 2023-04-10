e366b904a551715d2f177f9ec53e73d037444844
Ulrich Dinh <duong0028@gmail.com>
2023-04-10 22:04:59 +0300

    config store

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,27 @@
 import { configureStore, Action } from '@reduxjs/toolkit'
-import { rootReducer, RootState } from "./redux/index.ts";
+import { rootReducer, RootState } from "./redux/index.js";
 import thunk from "redux-thunk"
-// import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
-// import { ThunkAction, } from 'redux-thunk'
+import { ThunkAction, } from 'redux-thunk'
 
 // const baseURL = BACKEND_BASE_URL || "http://localhost:8000";
 // const baseURL = "http://localhost:8000";
 
-// export type AppDispatch = typeof store.dispatch
-// export type AppThunk = ThunkAction<void, RootState, null, Action<string>>
-// export const useAppDispatch = () => useDispatch<AppDispatch>()
+export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  null,
+  Action<string>
+>;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 
 // Create redux store
 export const store = configureStore({
   reducer: rootReducer,
   middleware: [thunk],
   devTools: process.env.NODE_ENV !== 'production',
-
 })
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     // include: ['src/frontend',]
+  },
+  define: {
+    'process.env': process.env
   }
 });
