8dc97b7ef7fe8a3d0f32423086089069fb8e5533
Ulrich Dinh <duong0028@gmail.com>
2023-04-14 10:45:36 +0300

    .

---
+++ src/components/Paginate.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Paginate = (props: any) => {
+  const { currentPage, totalPages, paginate } = props;
+  const [page, setPage] = useState<number>();
+  const [totalPageNumber, setTotalPageNumber] = useState();
+
+  useEffect(() => {
+    setPage(currentPage);
+    setTotalPageNumber(totalPages);
+  }, [currentPage, totalPages]);
+
+  return (
+    <>
+      <span className="pageCount">
+        {page} - {totalPageNumber}
+      </span>
+      <button className={page > 1 ? 'paginate-button' : 'paginate-button disable'} onClick={() => paginate('prev')}>
+        Prev
+      </button>
+      <button className={page === totalPageNumber ? 'paginate-button disable' : 'paginate-button'} onClick={() => paginate('next')}>
+        Next
+      </button>
+    </>
+  );
+};
+
+
+
+export default Paginate;
